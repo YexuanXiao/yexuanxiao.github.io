e03b35c3db8a3bb75ab8e4063b93636f72d54a62
YexuanXiao <bizwen@nykz.org>
2024-04-26 17:36:22 +0800

    Update components.js

--- assets/js/components.js
+++ assets/js/components.js
@@ -217,6 +217,7 @@ for (let element of document.body.querySelectorAll('div.highlight')) {
 	header.appendChild(icon)
 	let code = element.querySelector('.highlight')
 	code.firstChild.style.borderRadius = '0 0 6px 6px'
+	code.firstChild.style.lineHeight = '1.15em'
 	let button = document.createElement('span')
 	button.className = 'sw sw-document'
 	header.appendChild(button)
@@ -238,6 +239,7 @@ for (let element of document.body.querySelectorAll('pre>code[class]')) {
 	let code = element
 	element = element.parentNode
 	element.style.borderRadius = '0 0 6px 6px'
+	element.style.lineHeight = '1.15em'
 	let father = document.createElement('div')
 	father.className = 'message is-primary mt-3 mb-0'
 	father.style.borderRadius = '6px'
