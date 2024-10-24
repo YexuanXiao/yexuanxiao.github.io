1f6b2775e12313efd76798c462d9335d18cd1d21
YexuanXiao <bizwen@nykz.org>
2024-10-25 03:35:41 +0800

    fix viewport

--- assets/js/adjust.js
+++ assets/js/adjust.js
@@ -4,15 +4,11 @@
 	const postText = document.body.querySelectorAll('.post-text');
 	const postTextWidth = postText.offsetWidth;
 	const videoHeight = postTextWidth * 9 / 16;
-
-	const bilivideo = () => {
+	if (window.visualViewport.width <= 768) {
 		const elements = document.getElementsByClassName('bilivideo');
 		Array.prototype.forEach.call(elements, function (element) {
 			element.style.height = videoHeight + 'px';
 			element.style.margin = '.5em 0 .5em 0';
 		});
-	}
-	if (matchMedia('(max-width: 768px)').matches) {
-		bilivideo();
 	};
 }
